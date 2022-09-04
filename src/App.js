00d1ea9f0e7513ab251583ae29ccacc9c6e94ffe
RussellShire <russell.shire@gmail.com>
2022-09-04 11:15:05 +0100

    Add functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
+import "./styles.css";
+import { useState } from 'react';
+
 function App() {
+  const [calc, setCalc] = useState('');
+  const [result, setResult] = useState('');
+  
+  const ops = ['÷', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if(
+      (ops.includes(value) && calc === '') ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value)
+  }
+
   return (
     <div className='calculator-grid'>
       <div className="output">
-          <div className="previous-operand"></div>
-          <div className="current-operand"></div>
+          <div className="previous-operand">{ result || '0'}</div>
+          <div className="current-operand">{ calc || '0' }</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <button onClick={() => updateCalc('÷')}>÷</button>
+      <button onClick={() => updateCalc('1')}>1</button>
+      <button onClick={() => updateCalc('2')}>2</button>
+      <button onClick={() => updateCalc('3')}>3</button>
+      <button onClick={() => updateCalc('*')}>*</button>
+      <button onClick={() => updateCalc('4')}>4</button>
+      <button onClick={() => updateCalc('5')}>5</button>
+      <button onClick={() => updateCalc('6')}>6</button>
+      <button onClick={() => updateCalc('+')}>+</button>
+      <button onClick={() => updateCalc('7')}>7</button>
+      <button onClick={() => updateCalc('8')}>8</button>
+      <button onClick={() => updateCalc('9')}>9</button>
+      <button onClick={() => updateCalc('-')}>-</button>
+      <button onClick={() => updateCalc('.')}>.</button>
+      <button onClick={() => updateCalc('0')}>0</button>
+
       <button className="span-two">=</button>
     </div>
   )
