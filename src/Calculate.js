d9b7c68be76d48068f26fa46c3c746eb5e6a72d2
RussellShire <russell.shire@gmail.com>
2022-11-27 20:16:06 +0000

    Add work to convert Eval in Calculate funct

--- src/Calculate.js
+++ src/Calculate.js
@@ -1,16 +1,22 @@
 import { ops } from "./App";
 
 const calculate = (calc) => {
-    let operation = [];
+    // let operation = [];
+    // [...calc].forEach((char) => {
+    //     if(ops.includes(char)){
+    //         operation = operation + char
+    //     } else {
+    //         operation = operation + JSON.stringify(char)
+    //     }
+    // })
+    // console.log(operation)
 
-    [...calc].forEach((char) => {
-        if(ops.includes(char)){
-            operation = operation + char
-        } else {
-            operation = operation + JSON.stringify(char)
-        }
-    })
-    console.log(operation)
+    let split = calc.split(/[/*+-]+/) //jumping into Regex to split by multiple delminiators
+    split = split.map(item => Number(item)) // convert string to Number
+    const splitOps = calc.split(/[0.0-9]+/)
+    console.log(split)
+    console.log(splitOps)
+
 }
 
 export default calculate;
