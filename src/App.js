8905a6763eec8bfadbb5aee5497c4323c50f112c
RussellShire <russell.shire@gmail.com>
2022-11-29 13:15:00 +0000

    Tidy up App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./styles.css";
 import { React, useState } from 'react';
 import evaluateString from "./calculate";
 
-export const ops = ['/', '*', '+', '-', '.'];
-
-
+const ops = ['/', '*', '+', '-', '.'];
 
 function App() {
   const [calc, setCalc] = useState('');
