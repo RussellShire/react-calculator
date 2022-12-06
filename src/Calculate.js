bfaed402ca7ca689e7042c557308d842a07db749
RussellShire <russell.shire@gmail.com>
2022-12-06 13:02:36 +0000

    Remove comments from calculate component Keep copy of component with comments

--- src/Calculate.js
+++ src/Calculate.js
@@ -1,41 +1,38 @@
 const evaluateString = (calc) => {
-    let nums = calc.split(/[/*+-]+/) // Regex to turn string into an array of numbers by splitting by multiple delminiators
-    nums = nums.map(item => Number(item)) // convert strings to Numbers
+    let nums = calc.split(/[/*+-]+/) 
+    nums = nums.map(item => Number(item)) 
 
-    const operations = calc.split(/[0.0-9]+/) // use Regex to split by numbers and make an array of operations
-    let operationsClone = structuredClone(operations) // deep copy of array of operations for later manipulation within maps
+    const operations = calc.split(/[0.0-9]+/) 
+    let operationsClone = structuredClone(operations)
 
-    // console.log(nums)
-    // console.log(operations)
-
-    operations.map(op => { // mapping over the operations
-        if(op === '*' || op === '/'){ // first map checking for multiplaction or divide (because of order of operations)
-            let mapIndex = operationsClone.indexOf(op)  // creating an index from the clone because the cloned array will be manipulated
+    operations.map(op => { 
+        if(op === '*' || op === '/'){ 
+            let mapIndex = operationsClone.indexOf(op) 
             
-            if(op === '*') { // if statement to check if operation is multiply or divide
-                nums[mapIndex] = nums[mapIndex-1] * nums[mapIndex] // performs the operation on the numbers either side of the operation and sets it to the rightmost operation
+            if(op === '*') { 
+                nums[mapIndex] = nums[mapIndex-1] * nums[mapIndex]
             } else {
                 nums[mapIndex] = nums[mapIndex-1] / nums[mapIndex]
             }
             
-            nums.splice(mapIndex-1, 1) // removes the first number the operation was performed on leaving only the calcualted result
-            operationsClone.splice(operationsClone.indexOf(op), 1) // removes the operation that has just been performed from the cloned array
+            nums.splice(mapIndex-1, 1) 
+            operationsClone.splice(operationsClone.indexOf(op), 1) 
         } 
         return nums
     })
 
     operations.map(op => { 
-        if(op === '+' || op === '-'){ // second map checking for addition or subtraction (because of order of operations)
-            let mapIndex = operationsClone.indexOf(op)  // creating an index from the clone because the cloned array will be manipulated
+        if(op === '+' || op === '-'){ 
+            let mapIndex = operationsClone.indexOf(op)  
             
-            if(op === '+') { // if statement to check if operation is addition or subtraction
-                nums[mapIndex] = nums[mapIndex-1] + nums[mapIndex] // performs the operation on the numbers either side of the operation and sets it to the rightmost operation
+            if(op === '+') { 
+                nums[mapIndex] = nums[mapIndex-1] + nums[mapIndex] 
             } else {
                 nums[mapIndex] = nums[mapIndex-1] - nums[mapIndex]
             }
             
-            nums.splice(mapIndex-1, 1) // removes the first number the operation was performed on leaving only the calcualted result
-            operationsClone.splice(operationsClone.indexOf(op), 1) // removes the operation that has just been performed from the cloned array
+            nums.splice(mapIndex-1, 1) 
+            operationsClone.splice(operationsClone.indexOf(op), 1) 
         }
         return nums 
     })
